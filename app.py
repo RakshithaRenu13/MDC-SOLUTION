bd53ecc62ac8759aa344c5b0b64551c0fc22d3a6
Rakshitha Renu.S <rakshitha26.mvit@gmail.com>
2026-09-07 11:54:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(
     page_title="MDC Solution",
     page_icon="🏢",
     layout="wide",
-    initial_sidebar_state="expanded",
+    # initial_sidebar_state="expanded",
 )
 # ============================================================
 # EATON MDC HEADER
